2d587c4a14ad4ace1dbccf05e0f94ce8f5e51059
mhidusti <mhidusti@gmail.com>
2025-09-22 21:30:17 +0330

    add front

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      {/* Converted from original HTML */}
+      {/*
+        Paste your original HTML body content here as JSX.
+        Remember to adjust class -> className and self-close tags.
+      */}
+    </>
+  );
+}
+
+export default App;
